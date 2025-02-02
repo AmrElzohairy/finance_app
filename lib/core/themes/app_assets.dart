56e2f2b9fe15d7a200574bb558aed6691389e51d
amrElzohairy <aelzohairy9@gmail.com>
2025-02-02 14:12:51 +0200

    Add background image and update asset configuration
    - Added a new background image `bgImage.png` to the assets.
    - Created `app_assets.dart` to manage asset paths.
    - Updated `pubspec.yaml` to include the new image in the assets section and configured `flutter_assets` for automatic asset generation.

---
+++ lib/core/themes/app_assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBgImage
+  /// assets/images/bgImage.png
+  static const String imagesBgImage = "assets/images/bgImage.png";
+}
+
