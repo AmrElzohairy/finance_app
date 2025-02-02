31a1a3f6f9b9c9302f928af74c9562c71819c186
amrElzohairy <aelzohairy9@gmail.com>
2025-02-02 15:24:01 +0200

    Add social media login icons and update login view
    Added SVG images for Apple, Facebook, and Google logos. Updated the `AppAssets` class to include paths for these new images. Modified the `LoginView` to include buttons for social media login options and a registration prompt.

--- lib/core/themes/app_assets.dart
+++ lib/core/themes/app_assets.dart
@@ -2,8 +2,20 @@
 class Assets {
   Assets._();
   
+  /// Assets for imagesApple
+  /// assets/images/apple.svg
+  static const String imagesApple = "assets/images/apple.svg";
+
   /// Assets for imagesBgImage
   /// assets/images/bgImage.png
   static const String imagesBgImage = "assets/images/bgImage.png";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.svg
+  static const String imagesFacebook = "assets/images/facebook.svg";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.svg
+  static const String imagesGoogle = "assets/images/google.svg";
 }
 
--- lib/feature/auth/presentation/ui/login_view.dart
+++ lib/feature/auth/presentation/ui/login_view.dart
@@ -1,6 +1,8 @@
+import 'package:finance_app/core/themes/app_assets.dart';
 import 'package:finance_app/core/widgets/back_button.dart';
 import 'package:finance_app/core/widgets/spacing.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../../../core/widgets/custom_text_field.dart';
 import '../../../../core/widgets/primary_button.dart';
 import 'widgets/or_login_with.dart';
@@ -57,6 +59,99 @@ class LoginView extends StatelessWidget {
               PrimayButtonWidget(onPress: () {}, buttonText: 'Login'),
               HeightSpace(15),
               OrLoginWith(),
+              HeightSpace(22),
+              Row(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 56,
+                      width: 105,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 1, color: Color(0xFFE8ECF4)),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: Center(
+                        child: SvgPicture.asset(Assets.imagesFacebook),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    onTap: () {},
+
+                    child: Container(
+                      height: 56,
+                      width: 105,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 1, color: Color(0xFFE8ECF4)),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: Center(
+                        child: SvgPicture.asset(Assets.imagesGoogle),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    onTap: () {},
+
+                    child: Container(
+                      height: 56,
+                      width: 105,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 1, color: Color(0xFFE8ECF4)),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: Center(
+                        child: SvgPicture.asset(Assets.imagesApple),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Don’t have an account? ',
+                        style: TextStyle(
+                          color: Color(0xFF617AFD),
+                          fontSize: 15,
+                          fontFamily: 'Urbanist',
+                          fontWeight: FontWeight.w500,
+                          height: 1.40,
+                          letterSpacing: 0.15,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Register Now',
+                        style: TextStyle(
+                          color: Color(0xFF202854),
+                          fontSize: 15,
+                          fontFamily: 'Urbanist',
+                          fontWeight: FontWeight.w700,
+                          height: 1.40,
+                          letterSpacing: 0.15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ),
