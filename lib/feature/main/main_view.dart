645ebee22acdbc6c2c29d6877999d726bf241efd
amrElzohairy <aelzohairy9@gmail.com>
2025-02-02 21:12:36 +0200

    Add bottom navigation bar to MainView
    Implemented a bottom navigation bar with five items: Home, Statistics, Add (custom icon), My Card, and Profile. Added necessary imports for `flutter_screenutil` and `app_colors`. The navigation bar switches between different screens based on the selected item.

--- lib/feature/main/main_view.dart
+++ lib/feature/main/main_view.dart
@@ -1,4 +1,6 @@
+import 'package:finance_app/core/themes/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
@@ -9,8 +11,69 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  int _index = 0;
+  List<Widget> screens = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.yellow),
+    Container(color: Colors.purple),
+    Container(color: Colors.blue),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.grey,
+        elevation: 0.5,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'statistics',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              height: 50.h,
+              width: 50.w,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Container(
+                  height: 22.h,
+                  width: 22.w,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.add,
+                      color: AppColors.primaryColor,
+                      size: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: 'My Card',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
   }
 }
