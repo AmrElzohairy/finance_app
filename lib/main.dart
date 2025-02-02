a61e3407aef37e9bae458deb15dd48541a2238a2
amrElzohairy <aelzohairy9@gmail.com>
2025-02-02 13:49:07 +0200

    initial App

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const FinanceApp());
+}
+
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(title: 'Flutter Demo', home: Scaffold()));
+  }
+}
